4db2b63dbe02f278d365713db08d1755c41dc389
xiaomikasa <970911356@qq.com>
2019-08-22 09:56:01 +0800

    增加静态文件

--- src/main/java/com/hm/travel/TravelApplication.java
+++ src/main/java/com/hm/travel/TravelApplication.java
@@ -11,7 +11,6 @@ public class TravelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelApplication.class, args);
-        System.out.println(1);
     }
 
 }
