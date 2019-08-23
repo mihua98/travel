f4f874fd37b0ae67626e3958c479beffe16a33b1
xiaomikasa <970911356@qq.com>
2019-08-23 10:56:42 +0800

    新增了用户列表页面

--- src/main/java/com/hm/travel/controller/AdminController.java
+++ src/main/java/com/hm/travel/controller/AdminController.java
@@ -45,7 +45,7 @@ public class AdminController {
         Admin admin1 = adminService.AdminLogin(admin);
         if (null != admin1) {
             request.getSession().setAttribute("ADMIN",admin1);
-            return "all-admin-index";
+            return "adminPage/all-admin-index";
         }else{
             // TODO: 2019/8/22  !!!!!!!!!!!!!!!!!!!!!
             return "redirect:/404.html";
