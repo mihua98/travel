dc083f211181ef0dd79108b6b7c91348ff15c173
xiaomikasa <970911356@qq.com>
2019-08-28 17:21:14 +0800

    完善了跟团游详情页

--- src/main/java/com/hm/travel/controller/UserController.java
+++ src/main/java/com/hm/travel/controller/UserController.java
@@ -202,7 +202,7 @@ public class UserController {
         System.out.println("反回对象" + account1);
         if (null != account1) {
             request.getSession().setAttribute("user", account1.getUserInfo());
-            return "userPage/index";
+            return "redirect:/index";
         } else {
             // TODO: 2019/8/22 404!!!!!!!!!!!!!!!!!!!
             return "redirect:/404.html";
